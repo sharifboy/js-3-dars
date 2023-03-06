2fd172c0bc7391a6cb0734b8e7cde9d99c122d67
sharifboy <sharifboy@gmail.com>
2023-03-06 21:44:57 +0500

    made initial layouts

---
+++ app.js
@@ -0,0 +1,30 @@
+// let fname = "John Doe"
+
+// function toUppercase(text) {
+//     return text.toLowerCase();
+// }
+
+// console.log(toUppercase(fname));
+
+// let text = prompt("ismingizni kiriting")
+
+// function foo(text) {
+//     return text.split("").reverse().join("")
+// }
+
+// console.log(foo(text))
+
+let foo = prompt("enter text")
+
+function text() {
+    if (foo.length > 100){
+        alert()
+    }
+
+        return foo.split("").reverse().join("").toLowerCase()
+}
+
+console.log(text(foo));
+
+
+// prompt orqali matn kiritiladi. Kiritilgan matn funksiya orqali tekwiriladi. Agar matnda katta harf bo'lsa hammasi kichikka o'tadi. Reverse bo'ladi lenght alertda chiqadi tekshirilgan text consoleda chiqadi agar matn lenght i 100 dan katta bo'lsa consolda "Siz 100 harfdan katta matn kiritdingiz!" //
